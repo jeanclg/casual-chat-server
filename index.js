c3131d21b1bba7e0baa902fc4091c80356343150
Jean Garcia <jean_clg@hotmail.com>
2021-05-27 14:36:46 -0300

    fix cors error

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+const cors = require("cors");
 
 const router = require("./router");
 
@@ -8,12 +9,19 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+
+corsOptions = {
+  cors: true,
+  origins: ["http://localhost:3000"],
+};
+const io = socketio(server, corsOptions);
 
 app.use(router);
 
 io.on("connection", (socket) => {
-  console.log("New connection!");
+  socket.on("join", ({ name, room }, cb) => {
+    console.log(name, room);
+  });
 
   socket.on("disconnect", () => {
     console.log("User had left!");
