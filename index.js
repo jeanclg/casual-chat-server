87e2f31655c5aca006f6b0568b8729b79711cb3a
Jean Garcia <jean_clg@hotmail.com>
2021-05-27 15:44:04 -0300

    create user object and socket messages

--- index.js
+++ index.js
@@ -4,23 +4,56 @@ const http = require("http");
 const cors = require("cors");
 
 const router = require("./router");
+const { addUser, removeUser, getUser, getUserInRoom } = require("./user");
 
+// Porta que rodará o servidor
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Inicia o servidor via websocket
 const server = http.createServer(app);
 
+// Garante que apenas a nossa página poderá acessar o servidor
 corsOptions = {
   cors: true,
   origins: ["http://localhost:3000"],
 };
+
+// instancia o socket.io no nosso servidor com nossa página
 const io = socketio(server, corsOptions);
 
 app.use(router);
 
 io.on("connection", (socket) => {
   socket.on("join", ({ name, room }, cb) => {
-    console.log(name, room);
+    // Assim que entra na sala é criado o usuario a partir dos dados da pagina Join com o id gerado automaticamente pelo socket.io
+    const { error, user } = addUser({ id: socket.id, name, room });
+    // Caso de algum erro na criação do usuario é emitido uma mensagem de erro pela callback
+    if (error) return cb(error);
+
+    // Emite uma mensagem para o usuario que acabou de logar no room
+    socket.emmit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to ${user.room}`,
+    });
+
+    // Emite uma mensagem para todos os outros usuarios do room que entrou mais uma pessoa
+    socket.broadcast
+      .to(user.room)
+      .emmit("message", { user: "admin", text: `${user.name} has joined` });
+
+    socket.join(user.room);
+
+    cb();
+  });
+
+  socket.on("sendMessage", (message, cb) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emmit("message", { user: user.name, text: message });
+
+    cb();
   });
 
   socket.on("disconnect", () => {
---
+++ user.js
@@ -0,0 +1,37 @@
+const users = [];
+
+const addUser = ({ id, name, room }) => {
+  // Concatena o nome e o room e deixa tudo minusculo
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  // Pega o usuario que esta sendo criado e faz um filtro se o mesmo ja existe na lista de usuarios ja cadastrados
+  const existingUser = users.find((x) => x.name === name && x.room === room);
+
+  // Caso exista o usuario retorna uma mensagem de erro e impede de ser criado um novo usuario
+  if (existingUser) return { error: "Username is taken" };
+
+  // Cria um novo usuario
+  const user = { id, name, room };
+
+  // Coloca o usuario novo na lista de usuarios
+  users.push(user);
+
+  return { user };
+};
+
+const removeUser = (id) => {
+  // Pego o indice do usuario que tem o mesmo id que quero que seja removido
+  const index = users.findIndex((x) => x.id === id);
+
+  // Caso encontrado o usuario, remove ele da lista de usuarios
+  if (index !== -1) return users.splice(index, 1)[0];
+};
+
+// Pega o usuario a partir do id
+const getUser = (id) => users.find((x) => x.id === id);
+
+// Pega todos os usuarios que estão no mesmo room
+const getUsersInRoom = (room) => users.find((x) => x.room === room);
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
